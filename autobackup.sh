fbb74139e53a3f322032c6e712b7539046496aad
Proudmane <kylelawhorn@gmail.com>
2018-10-24 15:33:20 -0400

    Fixed sleep value again

--- autobackup.sh
+++ autobackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux send -t minecraft_server.0 'say Server will automatically reboot and backup in ten minutes. Please get to a safe place and disconnect.' ENTER
-sleep 10m
+sleep 9m
 tmux send -t minecraft_server.0 'say Server is going down in one minute. Please disconnect now.' ENTER
 sleep 1m
 ./stop.sh
