87c5b29a1a9795ec87086fa660c0931ec74e01b3
Proudmane <kylelawhorn@gmail.com>
2018-10-24 15:30:10 -0400

    Added needed cronjob and fixed autobackup sleep value

--- autobackup.sh
+++ autobackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux send -t minecraft_server.0 'say Server will automatically reboot and backup in ten minutes. Please get to a safe place and disconnect.' ENTER
-sleep 1m
+sleep 10m
 tmux send -t minecraft_server.0 'say Server is going down in one minute. Please disconnect now.' ENTER
 sleep 1m
 ./stop.sh
